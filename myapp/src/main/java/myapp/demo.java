b61d941826bdda27851b6e2f268b9bfef8a1cd30
maitri6 <maitrijainbishwal@gmail.com>
2021-08-16 18:33:55 +0530

    commited

--- myapp/src/main/java/myapp/demo.java
+++ myapp/src/main/java/myapp/demo.java
@@ -1,7 +1,11 @@
 package myapp;
 
 public class demo {
-      public static void main(String[] args) {
-		System.out.println("hello");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to github");
+
 	}
+
 }
