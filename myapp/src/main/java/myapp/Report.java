2a22c9db1c7aef4bb21af0eb11793cd42af87cff
maitri6 <maitrijainbishwal@gmail.com>
2021-08-17 12:21:20 +0530

    details added.

--- myapp/src/main/java/myapp/Report.java
+++ myapp/src/main/java/myapp/Report.java
@@ -3,10 +3,12 @@ package myapp;
 public class Report {
 
 	public static void main(String[] args) {
-		System.out.println("report pulling.");
+        System.out.println("report pulling.");
 		
 		System.out.println("master report");
 		System.out.println("master");
+		
+
 
 	}
 
